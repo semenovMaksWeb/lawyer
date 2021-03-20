15f8419619a6c00d2368f571200924e0001abf19
Павел <Pavel@mail.ru>
2021-03-20 18:03:31 +0400

    add our_attorneys

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
-import index from "~/index.pug"
-document.body.appendChild(index)
-import "~/scss/index.scss"
+import index from "~/index.pug";
+document.body.appendChild(index);
+import "~/scss/index.scss";
+import "~/assets/fonts/fonts.css";
